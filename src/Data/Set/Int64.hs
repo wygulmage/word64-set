1fa450c530e2517edf94687d00d0a9eadce1e2a3
Keith <wygulmage@users.noreply.github.com>
2022-02-24 01:34:11 -0500

    unified Word64 and Int64 Set APIs.

--- src/Data/Set/Int64.hs
+++ src/Data/Set/Int64.hs
@@ -2,49 +2,81 @@
            , BangPatterns
            , RankNTypes
            , ScopedTypeVariables
+           , FlexibleInstances
    #-}
 
 module Data.Set.Int64 (
-Set, empty, singleton, fromList,
-member, insert, delete,
+Int64Set, Set(..), singleton, fromList,
+insert, delete, alterF,
 intersection, union, difference,
+toAscList, toDesList,
 ) where
 
 import Data.Word (Word64)
 import Data.Int (Int64)
 import qualified Data.Set.Word64.Internal as Internal
 import qualified Data.Foldable as Foldable
-import Prelude hiding (foldl, foldr)
+import Prelude hiding (foldMap, foldl, foldr)
+import qualified GHC.Exts as Ext (build)
 
-data Set a where Set :: !Internal.Tree -> Set Int64
+type Int64Set = Set Int64
+data Set i64 where Set :: !Internal.Tree -> Set Int64
 
-instance Show (Set a) where
+instance Show (Set i64) where
    show si =
-     "fromList " <> show (Foldable.toList (observe si))
+     "fromList " <> show (toAscList (observe si))
 
 instance Eq (Set i64) where
    Set sx == Set sy = sx == sy
 
-observe :: Set a -> Set Int64
-observe si@(Set _) = si
-
-member :: a -> Set a -> Bool
-member !i (Set sx) = Internal.member (int64ToWord64 i) sx
-
-empty :: Set Int64
-empty = Set Internal.empty
+instance Semigroup (Set i64) where (<>) = union
+instance Monoid (Set Int64) where mempty = Set Internal.empty
 
 singleton :: Int64 -> Set Int64
 singleton = Set . Internal.singleton . int64ToWord64
 
 fromList :: [Int64] -> Set Int64
-fromList = Foldable.foldl' (flip insert) empty
+fromList = Foldable.foldl' (flip insert) mempty
 
 insert :: a -> Set a -> Set a
 insert !i (Set sx) = Set (Internal.insert (int64ToWord64 i) sx)
 
 delete :: a -> Set a -> Set a
 delete !i (Set sx) = Set (Internal.delete (int64ToWord64 i) sx)
+
+alterF :: (Functor m)=> (Bool -> m Bool) -> i64 -> Set i64 -> m (Set i64)
+alterF = alterFWith fmap
+
+alterFWith ::
+   forall m i64.
+   (forall x y. (x -> y) -> m x -> m y) ->
+   (Bool -> m Bool) ->
+   i64 -> Set i64 -> m (Set i64)
+{-^ @alterFWith@ provides a version of 'alterF' that you can use with an alternative 'Functor' class.
+-}
+alterFWith !mapper f = go -- Hopefully this makes specialization (e.g. in 'alterF') more reliable.
+   where
+      go i (Set sx) = choose `mapper` f member_
+         where
+            x = int64ToWord64 i
+            -- Reuse the information you have!
+            member_ = Internal.member x sx
+            (inserted, deleted)
+               | member_ = (sx, Internal.delete x sx)
+               | otherwise = (Internal.insert x sx, sx)
+            choose True = Set inserted
+            choose False = Set deleted
+{-# INLINE alterFWith #-}
+
+foldMap :: (Monoid b)=> (i64 -> b) -> Set i64 -> b
+foldMap f (Set sx) = case sx of
+   Internal.Branch pm l r
+      | Internal.suffixOf pm == Internal.suffixBitMask
+      -> Internal.foldMap f' r <> Internal.foldMap f' l
+   _
+      -> Internal.foldMap f' sx
+   where
+      f' = f . word64ToInt64
 
 foldr :: (a -> b -> b) -> b -> Set a -> b
 foldr f z (Set sx) = case sx of
@@ -53,6 +85,16 @@ foldr f z (Set sx) = case sx of
       -> Internal.foldr f' (Internal.foldr f' z l) r
    _
       -> Internal.foldr f' z sx
+   where
+      f' = f . word64ToInt64
+
+foldr' :: (a -> b -> b) -> b -> Set a -> b
+foldr' f z (Set sx) = case sx of
+   Internal.Branch pm l r
+      | Internal.suffixOf pm == Internal.suffixBitMask
+      -> Internal.foldr' f' (Internal.foldr' f' z l) r
+   _
+      -> Internal.foldr' f' z sx
    where
       f' = f . word64ToInt64
 
@@ -66,18 +108,33 @@ foldl f z (Set sx) = case sx of
    where
       f' z' x = f z' (word64ToInt64 x)
 
+foldl' :: (b -> a -> b) -> b -> Set a -> b
+foldl' f z (Set sx) = case sx of
+   Internal.Branch pm l r
+      | Internal.suffixOf pm == Internal.suffixBitMask
+      -> Internal.foldl' f' (Internal.foldl' f' z r) l
+   _
+      -> Internal.foldl' f' z sx
+   where
+      f' z' x = f z' (word64ToInt64 x)
+
 instance Foldable Set where
    null (Set sw) = Internal.null sw
+   foldMap = foldMap
    foldr = foldr
+   foldr' = foldr'
    foldl = foldl
+   foldl' = foldl'
    elem i (Set sw) = Internal.member (int64ToWord64 i) sw
    maximum = foldl (\ _ x -> x) (error "maximum: empty Set")
    minimum = foldr (\ x _ -> x) (error "minimum: empty Set")
 
-liftSet2 ::
-   (Internal.Tree -> Internal.Tree -> Internal.Tree) ->
-   Set i64 -> Set i64 -> Set i64
-liftSet2 f (Set sx) (Set sy) = Set (f sx sy)
+toAscList :: Set w64 -> [w64]
+toAscList xs = Ext.build (\ c n -> foldr c n xs)
+{-# INLINE [~0] toAscList #-}
+toDesList :: Set w64 -> [w64]
+toDesList xs = Ext.build (\ c n -> foldl (flip c) n xs)
+{-# INLINE [~0] toDesList #-}
 
 union :: Set i -> Set i -> Set i
 union = liftSet2 Internal.union
@@ -87,6 +144,14 @@ intersection = liftSet2 Internal.intersection
 
 difference :: Set i -> Set i -> Set i
 difference = liftSet2 Internal.difference
+
+observe :: Set a -> Set Int64
+observe si@(Set _) = si
+
+liftSet2 ::
+   (Internal.Tree -> Internal.Tree -> Internal.Tree) ->
+   Set i64 -> Set i64 -> Set i64
+liftSet2 f (Set sx) (Set sy) = Set (f sx sy)
 
 int64ToWord64 :: Int64 -> Word64
 int64ToWord64 = fromIntegral
--- src/Data/Set/Word64.hs
+++ src/Data/Set/Word64.hs
@@ -2,47 +2,53 @@
            , RankNTypes
            , ScopedTypeVariables
            , BangPatterns
+           , FlexibleInstances
    #-}
 
 
 module Data.Set.Word64 (
-Word64Set, Set (..), empty, singleton, fromList,
-member, insert, delete, alterF,
+Word64Set, Set (..), singleton, fromList,
+insert, delete, alterF,
 intersection, union, difference,
 toAscList, toDesList,
 ) where
 
 import Data.Foldable
-import Data.Maybe (fromMaybe)
 import Data.Word (Word64)
 import qualified Data.Set.Word64.Internal as Internal
 import qualified GHC.Exts as Ext (build)
 
 type Word64Set = Set Word64
 
-data Set a where Set :: !Internal.Tree -> Set Word64
+data Set w64 where Set :: !Internal.Tree -> Set Word64
+
+instance Show (Set w64) where
+   show sw =
+     "fromList " <> show (toAscList (observe sw))
+
+instance Eq (Set w64) where
+   Set sx == Set sy = sx == sy
+
+instance Semigroup (Set w64) where (<>) = union
+instance Monoid (Set Word64) where mempty = Set Internal.empty
 
 instance Foldable Set where
    null (Set sw) = Internal.null sw
+   foldMap f (Set sw) = Internal.foldMap f sw
    foldr f z (Set sw) = Internal.foldr f z sw
    foldr' f z (Set sw) = Internal.foldr' f z sw
    foldl f z (Set sw) = Internal.foldl f z sw
    foldl' f z (Set sw) = Internal.foldl' f z sw
-   foldMap f (Set sw) = Internal.foldMap f sw
-   maximum (Set sw) = fromMaybe (error "maximum: empty Set") (Internal.lookupMax sw)
-   minimum (Set sw) = fromMaybe (error "maximum: empty Set") (Internal.lookupMin sw)
+   elem w (Set sw) = Internal.member w sw
+   maximum = foldl (\ _ x -> x) (error "maximum: empty Set")
+   minimum = foldr (\ x _ -> x) (error "minimum: empty Set")
 
-empty :: Set Word64
-empty = Set Internal.Seed
 
 singleton :: Word64 -> Set Word64
 singleton x = Set (Internal.singleton x)
 
 fromList :: [Word64] -> Set Word64
 fromList ws = Set (Internal.fromList ws)
-
-member :: w64 -> Set w64 -> Bool
-member w (Set sw) = Internal.member w sw
 
 insert :: w64 -> Set w64 -> Set w64
 insert w (Set sw) = Set (Internal.insert w sw)
@@ -81,11 +87,6 @@ alterFWith !mapper f = go -- Hopefully this makes specialization (e.g. in 'alter
             choose False = Set deleted
 {-# INLINE alterFWith #-}
 
-liftSet2 ::
-   (Internal.Tree -> Internal.Tree -> Internal.Tree) ->
-   Set w64 -> Set w64 -> Set w64
-liftSet2 f (Set sx) (Set sy) = Set (f sx sy)
-
 union :: Set w64 -> Set w64 -> Set w64
 union = liftSet2 Internal.intersection
 
@@ -97,3 +98,11 @@ nonintersection = liftSet2 Internal.nonintersection
 
 difference :: Set w64 -> Set w64 -> Set w64
 difference = liftSet2 Internal.difference
+
+observe :: Set w64 -> Set Word64
+observe si@(Set _) = si
+
+liftSet2 ::
+   (Internal.Tree -> Internal.Tree -> Internal.Tree) ->
+   Set w64 -> Set w64 -> Set w64
+liftSet2 f (Set sx) (Set sy) = Set (f sx sy)
