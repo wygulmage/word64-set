0d057d1d29ae06c238ce4540584af067ea03d289
Keith <wygulmage@users.noreply.github.com>
2022-03-02 07:49:45 -0500

    reorganized tests

--- int64-set/test/Spec.hs
+++ int64-set/test/Spec.hs
@@ -1,63 +1,7 @@
 {-# LANGUAGE GADTs #-}
 
 import Test.Hspec
-import Test.QuickCheck
 import Data.Set.Int64.Spec
 
 main :: IO ()
-main = hspec $ do
-   describe "==" $ do
-      "sx == sx" `it` property prop_equal_self
-      "(toList sx == toList sy) == (sx == sy)" `it` property prop_toList_eq
-
-   describe "elem, insert, delete" $ do
-      "member singleton" `it` property prop_member_singleton
-      "member x (insert x sx)" `it` property prop_member_insert
-      "not (member x (delete x sx))" `it` property prop_member_delete
-
-   describe "alterF" $ do
-      "identity" `it` property prop_alterF_identity
-      "member" `it` property prop_alterF_member
-      "insert" `it` property prop_alterF_insert
-      "delete" `it` property prop_alterF_delete
-
-   describe "union" $ do
-      "commutative" `it` property prop_union_commutative
-      "self" `it` property prop_union_self
-      "empty" `it` property prop_union_empty
-
-   describe "intersection" $ do
-      "commutative" `it` property prop_intersection_commutative
-      "self" `it` property prop_intersection_self
-      "empty" `it` property prop_intersection_empty
-
-   describe "difference" $ do
-      "difference sx sx = empty" `it` property prop_difference_self
-      "difference empty" `it` property prop_difference_empty_1
-      "(`difference` empty)" `it` property prop_difference_empty_2
-
-   describe "splitMember" $ do
-      "member" `it` property prop_splitMember_member
-      "not member" `it` property prop_splitMember_not_member
-      "ordered" `it` property prop_splitMember_ordered
-
-   describe "to/from list" $ do
-      "identity" `it` property prop_to_from_list_identity
-
-   describe "toAscList" $ do
-      "sorted" `it` property prop_toAscList_sorted
-
-   describe "toDesList" $ do
-      "sorted" `it` property prop_toDesList_sorted
-
-   describe "maximum" $ do
-      "default" `it` property prop_maximum_default
-
-   describe "minimum" $ do
-      "default" `it` property prop_minimum_default
-
-   describe "null" $ do
-      "is empty" `it` property prop_null
-
-   describe "read, show" $ do
-      "read . show === id" `it` property prop_show_read
+main = hspec spec
--- word64-set/test/Data/Set/Word64/Spec.hs
+++ word64-set/test/Data/Set/Word64/Spec.hs
@@ -10,18 +10,83 @@ import Data.Word (Word64)
 import Data.List (sort, sortOn)
 import Data.Set.Word64
 import Test.QuickCheck
+import Test.Hspec
 
 instance (w64 ~ Word64)=> Arbitrary (Set w64) where
    arbitrary = fmap fromList arbitrary
 
+spec :: SpecWith ()
+spec = do
+   describe "==" $ do
+      "sx == sx" `it` property prop_equal_self
+      "(toList sx == toList sy) == (sx == sy)" `it` property prop_toList_eq
+
+   describe "elem, insert, delete" $ do
+      "elem is elem" `it` property prop_elem
+      "member singleton" `it` property prop_member_singleton
+      "delete deletes" `it` property prop_delete_elem
+      "delete x . insert x = id" `it` property prop_insert_delete
+
+   describe "alterF" $ do
+      "identity" `it` property prop_alterF_identity
+      "member" `it` property prop_alterF_member
+      "insert" `it` property prop_alterF_insert
+      "delete" `it` property prop_alterF_delete
+
+   describe "union" $ do
+      "`union sx sy` is the set of all elements of `sx` and all elements of `sy`" `it` property prop_union
+
+   describe "intersection" $ do
+      "`intersection sx sy` is the set of all elements that are shared between `sx` and `sy`" `it` property prop_intersection
+
+   describe "difference" $ do
+      "is difference" `it` property prop_difference
+
+   describe "splitMember" $ do
+      "member" `it` property prop_splitMember_member
+      "not member" `it` property prop_splitMember_not_member
+      "ordered" `it` property prop_splitMember_ordered
+
+   describe "to/from list" $ do
+      "identity" `it` property prop_to_from_list_identity
+
+   describe "toAscList" $ do
+      "sorted" `it` property prop_toAscList_sorted
+
+   describe "toDesList" $ do
+      "sorted" `it` property prop_toDesList_sorted
+
+   describe "maximum" $ do
+      "default" `it` property prop_maximum_default
+
+   describe "minimum" $ do
+      "default" `it` property prop_minimum_default
+
+   describe "null" $ do
+      "is empty" `it` property prop_null
+
+   describe "read, show" $ do
+      "read . show === id" `it` property prop_show_read
+
+
 prop_equal_self :: Word64Set -> Bool
 prop_equal_self sw = sw == sw
+prop_toList_eq :: Word64Set -> Word64Set -> Bool
+prop_toList_eq sx sy = (sx == sy) == (toList sx == toList sy)
 
+isElem f sx = all (`f` sx) (toList sx)
+prop_elem :: Word64Set -> Bool
+prop_elem = isElem elem
 prop_member_singleton w = elem w (singleton w)
-
-prop_member_insert w sw = elem w (insert w sw)
-
-prop_member_delete w sw = not (elem w (delete w sw))
+prop_insert_elem x sx = elem x (insert x sx)
+prop_delete_elem x sx = not (elem x (delete x sx))
+prop_insert_delete x sx =
+   elem x sx' && -- insert inserts.
+   not (elem x sx'') && -- delete deletes.
+   ((sx'' == sx)  /=  (sx' == sx)) -- insert only inserts and delete only deletes.
+   where
+      sx' = insert x sx
+      sx'' = delete x sx'
 
 prop_alterF_member w sw = getConst (alterF Const w sw) == elem w sw
 prop_alterF_insert w sw = runIdentity (alterF (\_-> Identity True) w sw) == insert w sw
@@ -32,15 +97,43 @@ prop_union_commutative sx sy = union sx sy == union sy sx
 prop_union_self si = union si si == si
 prop_union_empty si = union empty si == si
 
+prop_union :: Word64Set -> Word64Set -> Bool
+prop_union = isUnion union
+isUnion :: (Foldable m, Eq a)=> (m a -> m a -> m a) -> m a -> m a -> Bool
+isUnion f sx sy = let sz = f sx sy in
+   all (\ x -> elem x sx || elem x sy) sz &&
+   all (`elem` sz) sx &&
+   all (`elem` sz) sy
+
 
 prop_intersection_commutative sx sy = intersection sx sy == intersection sy sx
 prop_intersection_self si = intersection si si == si
 prop_intersection_empty si = intersection empty si == mempty
 
+prop_intersection :: Word64Set -> Word64Set -> Bool
+prop_intersection = isIntersection intersection
+isIntersection :: (Foldable m, Eq a)=> (m a -> m a -> m a) -> m a -> m a -> Bool
+isIntersection f sx sy = let sz = f sx sy in
+   all (\ x -> elem x sx && elem x sy) sz &&
+   all (\ x -> elem x sz /= not (elem x sy)) sx &&
+   all (\ x -> elem x sz /= not (elem x sx)) sy
+
+isNonintersection f sx sy =
+   all (\ x -> elem x sx /= elem x sy) sz &&
+   all (\ x -> elem x sz /= elem x sy) sx &&
+   all (\ x -> elem x sz /= elem x sx) sy
+   where sz = f sx sy
 
 prop_difference_self sx = difference sx sx == mempty
 prop_difference_empty_1 sx = difference mempty sx == mempty
 prop_difference_empty_2 sx = difference sx mempty == sx
+
+prop_difference :: Word64Set -> Word64Set -> Bool
+prop_difference = isDifference difference
+isDifference :: (Foldable m, Eq a)=> (m a -> m a -> m a) -> m a -> m a -> Bool
+isDifference f sx sy = let sz = f sx sy in
+   all (\ x -> elem x sx && not (elem x sy)) sz  &&
+   all (\ x -> elem x sz /= elem x sy) sx
 
 
 prop_splitMember_member x sx = elem x sx == case splitMember x sx of (_, b, _) -> b
@@ -70,9 +163,5 @@ prop_maximum_default sx =
 prop_null :: Word64Set -> Bool
 prop_null sx = null sx == (sx == mempty)
 
-
 prop_show_read :: Word64Set -> Bool
 prop_show_read sx = read (show sx) == sx
-
-prop_toList_eq :: Word64Set -> Word64Set -> Bool
-prop_toList_eq sx sy = (sx == sy) == (toList sx == toList sy)
